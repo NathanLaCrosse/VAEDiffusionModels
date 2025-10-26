e8204748b4335135d57a685830f21c403f15080d
Nathan L <mancool5672@gmail.com>
2025-10-26 13:52:48 -0500

    Changed the dataset to have values [-1, 1] for training purposes.

--- dataset.py
+++ dataset.py
@@ -2,7 +2,6 @@ import torch
 import os
 from torch import nn
 import numpy as np
-import matplotlib.pyplot as plt
 
 class ChurchData(nn.Module):
 
@@ -16,15 +15,17 @@ class ChurchData(nn.Module):
     def __getitem__(self, item):
         # Retrieve an item and convert it to a form ready for training
         # Format: (channels, rows, cols)
-        return torch.tensor(self.data[item]).permute(2, 0, 1)
+        im = self.data[item]
+        im = im / 255 * 2 - 1 # Scale into the interval [-1, 1]
+        return torch.tensor(im).permute(2, 0, 1)
 
-    def get_cleaned(self, item):
-        # Get an unedited form of an image
+    def get_clean(self, item):
         return self.data[item]
 
 
 dat = ChurchData()
 
 a = dat[0]
+b = dat.get_clean(0)
 
 print("hi")

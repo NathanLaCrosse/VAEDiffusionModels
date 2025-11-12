592831fe4d210483e9f8335d214b645284d59c8d
Nathan LaCrosse <mancool5672@gmail.com>
2025-11-11 22:26:19 -0600

    Trained a deeper model with EMA.

--- backwardsprocessdiffusion.py
+++ backwardsprocessdiffusion.py
@@ -9,6 +9,7 @@ import mushroomdata
 from unet import UNET
 from NathanVAE import VAE
 import matplotlib.pyplot as plt
+from torch_ema import ExponentialMovingAverage
 
 cpu = torch.device('cpu')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -17,7 +18,14 @@ vae = VAE(8)
 unet = UNET(64, 128, 100)
 
 vae.load_state_dict(torch.load("PTFiles/largernorm3.pt", map_location=device))
-unet.load_state_dict(torch.load("PTFiles/noreparam.pt", map_location=device))
+# unet.load_state_dict(torch.load("PTFiles/unconditionalref.pt", map_location=device)['model'])
+
+ema = ExponentialMovingAverage(unet.parameters(), decay=0.9999)
+
+checkpoint = torch.load("PTFiles/ema_deeper.pt")
+unet.load_state_dict(checkpoint['model'])
+ema.load_state_dict(checkpoint['ema'])
+
 vae = vae.to(device).eval()
 unet = unet.to(device).eval()
 
@@ -36,24 +44,26 @@ time_encodings = nc.positional_encoding(num_time_steps, 64).to(device)
 # print(alpha_bars[:5], alpha_bars[-5:])
 
 # Method to decode latent -> formula from class
-def denoise_latent(latent, unet, alphas, betas, alpha_bars, time_encodings, total_noise_steps, label):
+def denoise_latent(latent, unet, alphas, betas, alpha_bars, time_encodings, total_noise_steps):
     bs, _, _, _ = latent.size()
     pred = latent
 
     t = total_noise_steps
     with torch.no_grad():
-        while t > 0:
-            step_vect = time_encodings[t-1].unsqueeze(0).expand(bs, 64)
-            noise = unet(pred, step_vect, label)
+        with ema.average_parameters():
+            while t > 0:
+                step_vect = time_encodings[t-1].unsqueeze(0).expand(bs, 64)
+                
+                noise = unet(pred, step_vect)
 
-            pred = 1 / torch.sqrt(alphas[t-1]) * (pred - betas[t-1] / torch.sqrt(1 - alpha_bars[t-1]) * noise)
+                pred = 1 / torch.sqrt(alphas[t-1]) * (pred - betas[t-1] / torch.sqrt(1 - alpha_bars[t-1]) * noise)
 
-            if t > 1:
-                pred = pred + torch.sqrt(betas[t-1]) * torch.randn_like(pred)
+                if t > 1:
+                    pred = pred + torch.sqrt(betas[t-1]) * torch.randn_like(pred)
 
-            t -= 1
-        
-        return pred
+                t -= 1
+            
+            return pred
 
 
 stats = torch.load("latent_channel_info.pt")
--- unet.py
+++ unet.py
@@ -14,6 +14,8 @@ import mushroomdata
 from NathanVAE import VAE
 from torch.optim.lr_scheduler import LambdaLR
 
+from torch_ema import ExponentialMovingAverage
+
 class UNET(nn.Module):
 
     def __init__(self, time_embed_dim, label_embed_dim, num_classes, dropout_p=0.0):
@@ -40,31 +42,31 @@ class UNET(nn.Module):
         # )
 
         # Downward pass of the UNet
-        self.initial = nn.Conv2d(8, 16, 1) # 8 x 8 x 8 -> 16 x 8 x 8
-        self.downres1 = nc.NResBlocks(5, 16, 8, 8, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p) # 16 x 8 x 8
+        self.initial = nn.Conv2d(8, 32, 1) # 8 x 8 x 8 -> 16 x 8 x 8
+        self.downres1 = nc.NResBlocks(2, 32, 16, 8, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p) # 16 x 8 x 8
         
-        self.down1 = nn.Conv2d(16, 32, 3, 2, 1) # 16 x 8 x 8 -> 32 x 4 x 4
-        self.downres2 = nc.NResBlocks(4, 32, 16, 4, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p) # 32 x 4 x 4
+        self.down1 = nn.Conv2d(32, 64, 3, 2, 1) # 16 x 8 x 8 -> 32 x 4 x 4
+        self.downres2 = nc.NResBlocks(2, 64, 32, 4, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p) # 32 x 4 x 4
 
-        self.down2 = nn.Conv2d(32, 64, 3, 2, 1) # 32 x 4 x 4 -> 64 x 2 x 2
-        self.downres3 = nc.NResBlocks(3, 64, 32, 2, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p) # 64 x 2 x 2
+        self.down2 = nn.Conv2d(64, 128, 3, 2, 1) # 32 x 4 x 4 -> 64 x 2 x 2
+        self.downres3 = nc.NResBlocks(2, 128, 64, 2, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p) # 64 x 2 x 2
 
         # Upward pass of the UNet
-        self.upconv1 = nn.ConvTranspose2d(64, 32, 2, 2) # 64 x 2 x 2 -> 32 x 4 x 4
-        self.smoothing1 = nc.ResidualBlockWithEmbeddings(32, 16, 4, time_embed_dim=time_embed_dim,
+        self.upconv1 = nn.ConvTranspose2d(128, 64, 2, 2) # 64 x 2 x 2 -> 32 x 4 x 4
+        self.smoothing1 = nc.ResidualBlockWithEmbeddings(64, 32, 4, time_embed_dim=time_embed_dim,
                                                          label_embed_dim=label_embed_dim, dropout_p=dropout_p)
         # Concatenation happens here -> # 64 x 4 x 4 (in forward method)
-        self.reduce_channels1 = nn.Conv2d(64, 32, 1) # 64 x 4 x 4 -> 32 x 4 x 4
-        self.upres1 = nc.NResBlocks(4, 32, 16, 4, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p)
+        self.reduce_channels1 = nn.Conv2d(128, 64, 1) # 64 x 4 x 4 -> 32 x 4 x 4
+        self.upres1 = nc.NResBlocks(2, 64, 32, 4, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p)
 
-        self.upconv2 = nn.ConvTranspose2d(32, 16, 2, 2) # 32 x 4 x 4 -> 16 x 8 x 8
+        self.upconv2 = nn.ConvTranspose2d(64, 32, 2, 2) # 32 x 4 x 4 -> 16 x 8 x 8
         # Concatenation happens here -> 32 x 8 x 8
-        self.smoothing2 = nc.ResidualBlockWithEmbeddings(16, 8, 8, time_embed_dim=time_embed_dim,
+        self.smoothing2 = nc.ResidualBlockWithEmbeddings(32, 16, 8, time_embed_dim=time_embed_dim,
                                                          label_embed_dim=label_embed_dim, dropout_p=dropout_p)
-        self.reduce_channels2 = nn.Conv2d(32, 16, 1) # 32 x 8 x 8 -> 16 x 8 x 8
-        self.upres2 = nc.NResBlocks(5, 16, 8, 8, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p)
+        self.reduce_channels2 = nn.Conv2d(64, 32, 1) # 32 x 8 x 8 -> 16 x 8 x 8
+        self.upres2 = nc.NResBlocks(2, 32, 16, 8, time_embed_dim=time_embed_dim, label_embed_dim=label_embed_dim, dropout_p=dropout_p)
 
-        self.to_out = nn.Conv2d(16, 8, 1)
+        self.to_out = nn.Conv2d(32, 8, 1)
 
     def forward(self, x, time_embed):
         global_t = self.time_mlp(time_embed * 10)
@@ -124,6 +126,8 @@ def train_unet(epochs=15, batch_size = 32, learning_rate = 0.001, num_time_steps
         unet_model.load_state_dict(torch.load(load_file, map_location=device))
     unet_model = unet_model.to(device=device)
 
+    ema = ExponentialMovingAverage(unet_model.parameters(), decay=0.9999)
+
     time_encodings = nc.positional_encoding(num_time_steps, 64).to(device=device) # (num_time_steps, 64) array of time encodings
 
     loss_fn = nn.MSELoss(reduction="mean")
@@ -163,15 +167,16 @@ def train_unet(epochs=15, batch_size = 32, learning_rate = 0.001, num_time_steps
             loss.backward()
             torch.nn.utils.clip_grad_norm_(unet_model.parameters(), 5.0)
             optimizer.step()
+            ema.update()
             # scheduler.step()
 
             p_bar.set_postfix({
                 'Loss' : loss.item()
             })
 
-        torch.save(unet_model.state_dict(), f"PTFiles/{file_base}")
-        if (epoch + 1) % 50 == 0:
-            torch.save(unet_model.state_dict(), f"PTFiles/inprogress{epoch}{file_base}")
+        torch.save({'model' : unet_model.state_dict(), 'ema' : ema.state_dict()}, f"PTFiles/{file_base}")
+        if (epoch + 1) % 35 == 0:
+            torch.save({'model' : unet_model.state_dict(), 'ema' : ema.state_dict()}, f"PTFiles/inprogress{epoch}{file_base}")
 
 if __name__ == '__main__':
     # batch size could be too big (256 -> 64 -> 32?)
@@ -179,6 +184,7 @@ if __name__ == '__main__':
     # In diffusion process - sample from distribution generated by means (latent vectors)
     # Can select another scaling factor ~0.4 multiplied by the latent standard deviation
     # Gets closer convergence to the mean
-    train_unet(epochs=100, batch_size=32, file_base="unconditional.pt", num_time_steps=1000, learning_rate=1e-4, dropout=0.0)
+    train_unet(epochs=200, batch_size=64, file_base="ema_deeper.pt", num_time_steps=1000, learning_rate=1e-4, dropout=0.0)
+    # train_unet(epochs=200, batch_size=64, file_base="unconditionalref.pt", num_time_steps=1000, learning_rate=5e-5, dropout=0.0, load_file="PTFiles/unconditional.pt")
     # train_unet(epochs=50, batch_size=256, file_base="refined.pt", num_time_steps=1000, learning_rate=5e-7, dropout=0.0, load_file="PTFiles/thousand.pt")
     # train_unet(epochs=200, batch_size=256, file_base="thousand.pt", num_time_steps=100, learning_rate=1e-4)
